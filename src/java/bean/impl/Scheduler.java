536dedb3f46ed1250e647cf13e1730728ae88104
mengYan <myeanng@sina.com>
2016-05-31 16:32:47 +0800

    master# 在Spring中，既可以实现 InitializingBean和DisposableBean接口或在bean配置文件中指定 init-method 和 destroy-method 在初始化和销毁回调函数。在这篇文章中，我们将介绍如何使用 @PostConstruct 和 @PreDestroy 注解来做同样的事情。 注：@PostConstruct和@PreDestroy 标注不属于 Spring，它是在J2EE库- common-annotations.jar。默认情况下，Spring不会意识到@PostConstruct和@PreDestroy注解。要启用它，要么注册“CommonAnnotationBeanPostProcessor”，要么在bean配置文件的<context:annotation-config />‘ 指定，initIt()方法(@PostConstruct)被调用时，消息属性设置后 cleanUp() 方法(@PreDestroy)是在context.close()执行后被调用;

--- src/java/bean/impl/Scheduler.java
+++ src/java/bean/impl/Scheduler.java
@@ -1,13 +1,15 @@
 package bean.impl;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import bean.IBean;
 
 /**
- * 注释注入
- * 并指定作用域为原型
+ * 注释注入 并指定作用域为原型
+ * 
  * @author Administrator
  *
  */
@@ -20,6 +22,16 @@ public class Scheduler implements IBean {
 		this.name = name;
 	}
 
+	/**
+	 * 在Spring中，既可以实现 InitializingBean和DisposableBean接口或在bean配置文件中指定 init-method
+	 * 和 destroy-method 在初始化和销毁回调函数。在这篇文章中，我们将介绍如何使用 @PostConstruct和@PreDestroy
+	 * 注解来做同样的事情。 注：@PostConstruct和@PreDestroy 标注不属于 Spring，它是在J2EE库-
+	 * common-annotations.jar。
+	 * 默认情况下，Spring不会意识到@PostConstruct和@PreDestroy注解。要启用它，要么注册“
+	 * CommonAnnotationBeanPostProcessor”，要么在bean配置文件的
+	 * <context:annotation-config />‘ 指定，
+	 */
+	@PostConstruct
 	@Override
 	public void printHello() {
 		System.out.println("Spring 4 : Hello ! " + name);
